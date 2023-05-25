9aa5f79fda4ee2b863d676544203d0b443501b45
Vitaliy Roy <vitaliybrandet@gmail.com>
2023-05-25 15:48:21 +0300

    Добавьте файлы проекта.

---
+++ lr26/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace lr26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sender_txt = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.recipient_txt = new System.Windows.Forms.TextBox();
+            this.postal_code_txt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.date_txt = new System.Windows.Forms.TextBox();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sender_txt
+            // 
+            this.sender_txt.Location = new System.Drawing.Point(12, 12);
+            this.sender_txt.Name = "sender_txt";
+            this.sender_txt.Size = new System.Drawing.Size(399, 20);
+            this.sender_txt.TabIndex = 0;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 142);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Створити";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Адрес відправника";
+            // 
+            // recipient_txt
+            // 
+            this.recipient_txt.Location = new System.Drawing.Point(12, 38);
+            this.recipient_txt.Name = "recipient_txt";
+            this.recipient_txt.Size = new System.Drawing.Size(399, 20);
+            this.recipient_txt.TabIndex = 1;
+            // 
+            // postal_code_txt
+            // 
+            this.postal_code_txt.Location = new System.Drawing.Point(12, 116);
+            this.postal_code_txt.Name = "postal_code_txt";
+            this.postal_code_txt.Size = new System.Drawing.Size(399, 20);
+            this.postal_code_txt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Адрес отримувача";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Поштовий індекс";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Дата відправлення";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(417, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ініціали відправника";
+            // 
+            // date_txt
+            // 
+            this.date_txt.Location = new System.Drawing.Point(12, 90);
+            this.date_txt.Name = "date_txt";
+            this.date_txt.Size = new System.Drawing.Size(399, 20);
+            this.date_txt.TabIndex = 8;
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(12, 64);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(399, 20);
+            this.name_txt.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 171);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.date_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.postal_code_txt);
+            this.Controls.Add(this.recipient_txt);
+            this.Controls.Add(this.sender_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sender_txt;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox recipient_txt;
+        private System.Windows.Forms.TextBox postal_code_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox date_txt;
+        private System.Windows.Forms.TextBox name_txt;
+    }
+}
+
---
+++ lr26/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace lr26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fn = @"g:\123.dotx";
+        Word.Application word = new Word.Application();
+        Word.Document doc = new Word.Document();
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            doc.Close();
+            word.Quit();
+            doc = null;
+            word = null;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Object missingObj = System.Reflection.Missing.Value;
+            Object trueObj = true;
+            Object falseObj = false;
+            Object templatePathObj = fn;
+            doc = word.Documents.Add(ref templatePathObj, ref missingObj, ref missingObj, ref missingObj);
+            doc.Activate();
+            foreach (Word.FormField f in doc.FormFields)
+            {
+                switch (f.Name)
+                {
+                    case "sender":
+                        f.Range.Text = sender_txt.Text;
+                        break;
+                    case "recipient":
+                        f.Range.Text = recipient_txt.Text;
+                        break;
+                    case "postal_code":
+                        f.Range.Text = postal_code_txt.Text;
+                        break;
+                    case "name":
+                        f.Range.Text = name_txt.Text;
+                        break;
+                    case "date":
+                        f.Range.Text = date_txt.Text;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            word.Visible = true;
+        }
+    }
+}
